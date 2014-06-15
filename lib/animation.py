Xananax/dopey

commit d48524fff7dc32d88730a3d53e98e0d0d647fa29

various fixes

lib/animation.py
@@ -45,7 +45,7 @@ class Animation(object):
         self.edit_operation = None
         self.edit_frame = None
 
-    def clear_xsheet(self, doc, init=False):
+    def clear_xsheet(self, init=False):
         self.timeline = TimeLine(self.opacities)
         self.timeline.append_layer()
         self.cleared = True
@@ -155,14 +155,12 @@ class Animation(object):
         str_data = self.xsheet_as_str()
         xsheetfile.write(str_data)
 
-    def str_to_xsheet(self, doc, ani_data):
+    def str_to_xsheet(self, ani_data):
         """
         Update TimeLine from animation data.
     
         """
-
         data = json.loads(ani_data)
-
         # first check if it's in the legacy non-descriptive JSON or new XDNA format
         if type(data) is dict and data['XDNA']:
             x = self.xdna
@@ -170,7 +168,7 @@ class Animation(object):
             raster_frames = data['xsheet']['raster_frame_lists']
 
             self.timeline = TimeLine(self.opacities)
-            self.timeline.fps = data['xsheet']['framerate']
+            self.timeline.fps = int(data['xsheet']['framerate'])
             self.cleared = True
 
 
@@ -185,12 +183,10 @@ class Animation(object):
                     self.timeline[j].opacity = raster_frames[j]['opacity']
                     self.timeline[j].locked = raster_frames[j]['locked']
                     self.timeline[j].composite = raster_frames[j]['composite']
-                    for ui in raster_frames[j]:
-                        try:
-                            i = int(ui)
-                        except ValueError:
+                    for i in raster_frames[j]:
+                        if type(i) is not int:
                             continue
-                        d = raster_frames[j][ui]
+                        d = raster_frames[j][i]
                         f = self.timeline[j][i]
                         if d['idx'] is not None:
                             if d['idx'] < len(self.doc.layers):
@@ -245,13 +241,13 @@ class Animation(object):
                 self.timeline[0][i].cel = cel
             self.timeline.cleanup()
 
-    def _read_xsheet(self, doc, xsheetfile):
+    def _read_xsheet(self, xsheetfile):
         """
         Update FrameList from file.
     
         """
         ani_data = xsheetfile.read()
-        self.str_to_xsheet(doc, ani_data)
+        self.str_to_xsheet(ani_data)
     
     def save_xsheet(self, filename):
         root, ext = os.path.splitext(filename)
@@ -259,15 +255,15 @@ class Animation(object):
         xsheetfile = open(xsheet_fn, 'w')
         self._write_xsheet(xsheetfile)
     
-    def load_xsheet(self, doc, filename):
+    def load_xsheet(self, filename):
         root, ext = os.path.splitext(filename)
         xsheet_fn = root + '.xsheet'
         try:
             xsheetfile = open(xsheet_fn, 'r')
         except IOError:
-            self.clear_xsheet(doc)
+            self.clear_xsheet()
         else:
-            self._read_xsheet(doc, xsheetfile)
+            self._read_xsheet(xsheetfile)
     
     def save_png(self, filename, **kwargs):
         prefix, ext = os.path.splitext(filename)
@@ -478,26 +474,43 @@ class Animation(object):
         frame = self.timeline[lidx][idx]
         self.doc.do(anicommand.ToggleSkipVisible(self.doc, frame))
 
+#@TODO: frame movements done through command stack?
     def previous_frame(self, with_cel=False):
         new = self.timeline.goto_previous(with_cel)
+        cel = self.timeline.layer.cel_at(self.timeline.idx)
+        if cel is not None:
+            layer_idx = self.doc.layers.index(cel)
+            self.doc.layer_idx = layer_idx
         self.update_opacities()
         if new: self.cleared = True
         self.doc.call_doc_observers()
 
     def next_frame(self, with_cel=False):
         new = self.timeline.goto_next(with_cel)
+        cel = self.timeline.layer.cel_at(self.timeline.idx)
+        if cel is not None:
+            layer_idx = self.doc.layers.index(cel)
+            self.doc.layer_idx = layer_idx
         self.update_opacities()
         if new: self.cleared = True
         self.doc.call_doc_observers()
 
     def previous_keyframe(self):
         new = self.timeline.goto_previous_key()
+        cel = self.timeline.layer.cel_at(self.timeline.idx)
+        if cel is not None:
+            layer_idx = self.doc.layers.index(cel)
+            self.doc.layer_idx = layer_idx
         self.update_opacities()
         if new: self.cleared = True
         self.doc.call_doc_observers()
 
     def next_keyframe(self):
         new = self.timeline.goto_next_key()
+        cel = self.timeline.layer.cel_at(self.timeline.idx)
+        if cel is not None:
+            layer_idx = self.doc.layers.index(cel)
+            self.doc.layer_idx = layer_idx
         self.update_opacities()
         if new: self.cleared = True
         self.doc.call_doc_observers()

gui/timelinewindow.py
@@ -730,7 +730,6 @@ class TimelineTool(Gtk.VBox):
         self.set_size_request(200, -1)
 
         self.doc.doc_observers.append(self.update)
-        self.doc.doc_observers.append(self.sort_layers)
 
     def update(self, doc=None):
         self.data = self.ani.timeline
@@ -808,9 +807,6 @@ class TimelineTool(Gtk.VBox):
         x, y = hadj.get_value(), vadj.get_value()
         hadj.set_value(x + dx)
         vadj.set_value(y + dy)
-
-    def sort_layers(self, doc=None):
-        self.ani.sort_layers()
 
     def tool_widget_properties(self):
         d = TimelinePropertiesDialog(self, self.app)

lib/anicommand.py
@@ -160,6 +160,7 @@ class AddFrame(Action):
         self.frame.add_cel(self.layer)
         self._notify_canvas_observers([self.layer])
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
     
     def undo(self):
@@ -168,6 +169,7 @@ class AddFrame(Action):
         self.timeline[self.lidx].pop(self.idx)
         self._notify_canvas_observers([self.layer])
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 
 
@@ -193,6 +195,7 @@ class RemoveFrame(Action):
         self.removed = self.timeline[self.layer].remove_frames(self.idx)
         
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self.doc.ani.cleared = True
         self._notify_document_observers()
             
@@ -203,6 +206,7 @@ class RemoveFrame(Action):
             self.doc.layer_idx = self.prev_idx
             self._notify_canvas_observers([self.frame.cel])
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self.doc.ani.cleared = True
         self._notify_document_observers()
 
@@ -239,6 +243,7 @@ class PasteCel(Action):
         self.doc.ani.edit_frame = None
 
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 
     def undo(self):
@@ -250,6 +255,7 @@ class PasteCel(Action):
             self.doc.layer_idx -= 1
             self._notify_canvas_observers([self.doc.ani.edit_frame.cel])
         self.doc.ani.update_opacities()
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 
 
@@ -263,6 +269,7 @@ class InsertLayer(Action):
     def redo(self):
         self.timeline.insert_layer(idx=self.idx)
         self.doc.ani.cleared = True
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 
     def undo(self):
@@ -270,6 +277,7 @@ class InsertLayer(Action):
         if self.timeline.idx == len(self.timeline):
             self.timeline.idx -= 1
         self.doc.ani.cleared = True
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 
 
@@ -301,6 +309,7 @@ class RemoveLayer(Action):
 
         self.doc.ani.update_opacities()
         self.doc.ani.cleared = True
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
             
     def undo(self):
@@ -314,5 +323,6 @@ class RemoveLayer(Action):
 
         self.doc.ani.update_opacities()
         self.doc.ani.cleared = True
+        self.doc.ani.sort_layers()
         self._notify_document_observers()
 

lib/document.py
@@ -246,7 +246,7 @@ class Document():
             for f in self.canvas_observers:
                 f(*bbox)
 
-        self.ani.clear_xsheet(self, init)
+        self.ani.clear_xsheet(init)
         self.call_doc_observers()
 
     def get_current_layer(self):
@@ -1039,9 +1039,9 @@ class Document():
 
         try:
             ani_data = z.read('animation.xsheet')
-            self.ani.str_to_xsheet(self, ani_data)
+            self.ani.str_to_xsheet(ani_data)
         except KeyError:
-            self.ani.load_xsheet(self, filename)
+            self.ani.load_xsheet(filename)
 
         if selected_layer is not None:
             for i, layer in zip(range(len(self.layers)), self.layers):

lib/framelist.py
@@ -1,782 +1,0 @@
-# This file is part of MyPaint.
-# Copyright (C) 2007-2008 by Martin Renold <martinxyz@gmx.ch>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-
-from layer import Layer
-
-
-DEFAULT_OPACITIES = {
-    'cel': 1./2, # The inmediate next and previous cels
-    'key': 1./2, # The cel keys that are after and before the current cel
-    'inbetweens': 1./4, # The cels that are between the keys mentioned above
-    'other keys': 1./4, # The other keys
-    'other': 0,    # The rest of the cels
-}
-
-DEFAULT_ACTIVE_CELS = {
-    'cel': True,
-    'key': True,
-    'inbetweens': True,
-    'other keys': True,
-    'other': False,
-}
-
-DEFAULT_NEXTPREV = {
-    'next': True,
-    'previous': True,
-}
-
-class Frame(object):
-    def __init__(self, is_key=False, cel=None):
-        self.is_key = is_key
-        self.description = ""
-        self.cel = cel
-        self.skip_visible = False
-
-    def set_key(self):
-        self.is_key = True
-
-    def unset_key(self):
-        self.is_key = False
-
-    def toggle_key(self):
-        self.is_key = not self.is_key
-
-    def toggle_skip_visible(self):
-        self.skip_visible = not self.skip_visible
-
-    def add_cel(self, cel):
-        self.cel = cel
-
-    def remove_cel(self):
-        self.cel = None
-
-    def has_cel(self):
-        return self.cel is not None
-
-
-class FrameList(list):
-    """
-    The list of frames that constitutes an animation.
-
-    """
-    def __init__(self, length, doc=None, opacities=None, active_cels=None, nextprev=None, init=False):
-        self.append_frames(length)
-        if opacities is None:
-            opacities = {}
-        self.opacities = dict(DEFAULT_OPACITIES)
-        if active_cels is None:
-            active_cels = {}
-        self.active_cels = dict(DEFAULT_ACTIVE_CELS)
-        if nextprev is None:
-            nextprev = {}
-        self.nextprev = dict(DEFAULT_NEXTPREV)
-        self.setup_opacities(opacities)
-        self.setup_active_cels(active_cels)
-        self.setup_nextprev(nextprev)
-        if init:
-            self.setup_init_frame(doc)
-
-    def setup_init_frame(self, doc):
-        name = "<A> CEL 1"
-        cel = doc.layers[-1]
-        cel.name = name
-        self[0].add_cel(cel)
-        doc.call_doc_observers()
-
-    def setup_opacities(self, opacities):
-        self.opacities.update(opacities)
-        self.convert_opacities()
-
-    def set_opacityfactor(self, factor):
-        self.convert_opacities(factor)
-
-    def convert_opacities(self, factor=1):
-        self.converted_opacities = {}
-        for k, v in self.opacities.items():
-            self.converted_opacities[k] = v * factor
-
-    def setup_active_cels(self, active_cels):
-        self.active_cels.update(active_cels)
-
-    def setup_nextprev(self, nextprev):
-        self.nextprev.update(nextprev)
-
-    def append_frames(self, length):
-        for l in range(length):
-            self.append(Frame())
-
-    def remove_frames(self, length, at_end=False):
-        """
-        Remove frames from the current position or from the end.
-        """
-        removed = []
-        if at_end:
-            idx = len(self) - 1
-        else:
-            idx = self.idx
-        if idx + length > len(self):
-            length = len(self) - idx
-        for l in range(length):
-            removed.append(self.pop(idx))
-        if self.idx > len(self) - 1:
-            self.idx = len(self) - 1
-        return removed
-
-    def insert_frames(self, frames):
-        for f in frames:
-            self.insert(self.idx, f)
-
-    def insert_empty_frames(self, length):
-        for l in range(length):
-            self.insert(self.idx, Frame())
-
-    def get_selected(self):
-        return self[self.idx]
-
-    def select(self, n):
-        if not 0 <= n <= len(self)-1:
-            #raise IndexError("Trying to select inexistent frame.")
-            print "IndexError: Trying to select inexistent frame."
-            return
-        self.idx = n
-
-    def goto_next(self, with_cel=False):
-        if with_cel:
-            next_frame = self._get_next_frame_with_cel()
-            if next_frame is None:
-                #raise IndexError("There is no next frame with cel.")
-                print "IndexError: There is no next frame with cel."
-            else:
-                self.idx = self.index(next_frame)
-            return
-        if not self.has_next():
-            #raise IndexError("Trying to go to next at the last frame.")
-            print "IndexError: Trying to go to next at the last frame."
-        else:
-            self.idx += 1
-
-    def goto_previous(self, with_cel=False):
-        if with_cel:
-            prev_frame = self._get_previous_frame_with_cel()
-            if prev_frame is None:
-                #raise IndexError("There is no previous frame with cel.")
-                print "IndexError: There is no previous frame with cel."
-            else:
-                self.idx = self.index(prev_frame)
-            return
-        if not self.has_previous():
-            #raise IndexError("Trying to go to previous at the first frame.")
-            print "IndexError:Trying to go to previous at the first frame."
-        else:
-            self.idx -= 1
-
-    def has_next(self, with_cel=False):
-        if with_cel:
-            return self._get_next_frame_with_cel() is not None
-        if self.idx == len(self)-1:
-            return False
-        return True
-
-    def has_previous(self, with_cel=False):
-        if with_cel:
-            return self._get_previous_frame_with_cel() is not None
-        if self.idx == 0:
-            return False
-        return True
-
-    def get_next_key(self):
-        for f in (self[self.idx+1:]):
-            if f.is_key and not f.skip_visible:
-                return f
-        return None
-
-    def get_previous_key(self):
-        for f in reversed(self[:self.idx]):
-            if f.is_key and not f.skip_visible:
-                return f
-        return None
-
-    def goto_next_key(self):
-        f = self.get_next_key()
-        if f is None:
-            #raise IndexError("Trying to go to inexistent next keyframe.")
-            print "IndexError: Trying to go to inexistent next keyframe."
-        else:
-            self.idx = self.index(f)
-
-    def goto_previous_key(self):
-        f = self.get_previous_key()
-        if f is None:
-            print "Trying to go to inexistent previous keyframe."
-        else:
-            self.idx = self.index(f)
-
-    def has_next_key(self):
-        f = self.get_next_key()
-        if f is None:
-            return False
-        return True
-
-    def has_previous_key(self):
-        f = self.get_previous_key()
-        if f is None:
-            return False
-        return True
-
-    def cel_at(self, n):
-        """
-        Return the cel at the nth frame.
-
-        """
-        if self[n].cel is not None:
-            return self[n].cel
-        for f in reversed(self[:n]):
-            if f.cel is not None:
-                return f.cel
-        return None
-
-    def get_previous_cel(self):
-        """
-        Return the previous cel that is different than the cel of the
-        current frame.
-
-        """
-        prev_frame = self._get_previous_frame_with_cel()
-        if prev_frame is not None:
-            return prev_frame.cel
-        return None
-
-    def get_next_cel(self):
-        """
-        Return the next cel that is different than the cel of the
-        current frame.
-
-        """
-        next_frame = self._get_next_frame_with_cel()
-        if next_frame is not None:
-            return next_frame.cel
-        return None
-
-    def get_all_cels(self):
-        cels = []
-        for f in self:
-            if f.cel is not None and f.cel not in cels:
-                cels.append(f.cel)
-        return cels
-
-    def _get_previous_frame_with_cel(self):
-        """
-        Return the previous frame with a cel that is different than
-        the cel of the current frame.
-
-        """
-        cur_cel = self.cel_at(self.idx)
-        if not cur_cel:
-            return None
-        for f in reversed(self[:self.idx]):
-            if f.cel is not None and f.cel != cur_cel and not f.skip_visible:
-                return f
-        return None
-
-    def _get_next_frame_with_cel(self):
-        """
-        Return the next frame with a cel that is different than the
-        cel of the current frame.
-
-        """
-        cur_cel = self.cel_at(self.idx)
-        if not cur_cel:
-            return None
-        for f in (self[self.idx+1:]):
-            if f.cel is not None and f.cel != cur_cel and not f.skip_visible:
-                return f
-        return None
-
-    def cel_for_frame(self, frame):
-        """
-        Return the cel for the frame.
-
-        If no cel is defined for this frame, look back the frame list
-        for the first cel that appears.  The returned cel is the one
-        to be shown in the animation at the nth frame.
-
-        """
-        return self.cel_at(self.index(frame))
-
-    def get_opacities(self, idx=None):
-        """
-        Return a map of cels and the opacity they should have.
-
-        To draw the current cel, the artist have to see it 100%
-        opaque, and she may want to see the neighbour cels
-        transparented.
-
-        """
-        if idx is not None:
-            self.idx = idx
-
-        opacities = {}
-
-        def get_opa(nextprev, c):
-            can_nextprev = self.nextprev[nextprev]
-            if can_nextprev and self.active_cels[c]:
-                return self.converted_opacities[c]
-            return 0
-
-        # explicit skip of cels:
-        for frame in self:
-            if frame.skip_visible:
-                opacities[frame.cel] = 0
-
-        # current cel, always full opacity:
-        cel = self.cel_for_frame(self.get_selected())
-        if cel:
-            opacities[cel] = 1
-
-        # next:
-        cel = self.get_previous_cel()
-        if cel and cel not in opacities.keys():
-            opacities[cel] = get_opa('previous', 'cel')
-
-        # previous:
-        cel = self.get_next_cel()
-        if cel and cel not in opacities.keys():
-            opacities[cel] = get_opa('next', 'cel')
-
-        # previous key:
-        prevkey_idx = 0
-        if self.has_previous_key():
-            prevkey = self.get_previous_key()
-            prevkey_idx = self.index(prevkey)
-            cel = self.cel_for_frame(prevkey)
-            if cel and cel not in opacities.keys():
-                opacities[cel] = get_opa('previous', 'key')
-
-        # next key:
-        nextkey_idx = len(self)-1
-        if self.has_next_key():
-            nextkey = self.get_next_key()
-            nextkey_idx = self.index(nextkey)
-            cel = self.cel_for_frame(nextkey)
-            if cel and cel not in opacities.keys():
-                opacities[cel] = get_opa('next', 'key')
-
-        def has_cel(f):
-            return f.cel is not None
-
-        # inbetweens:
-        next_inbetweens_range = self[self.idx:nextkey_idx]
-        for frame in filter(has_cel, next_inbetweens_range):
-            cel = frame.cel
-            if cel not in opacities.keys():
-                opacities[cel] = get_opa('next', 'inbetweens')
-        prev_inbetweens_range = self[prevkey_idx:self.idx]
-        for frame in filter(has_cel, prev_inbetweens_range):
-            cel = frame.cel
-            if cel not in opacities.keys():
-                opacities[cel] = get_opa('previous', 'inbetweens')
-
-        # frames outside inmediate keys:
-        next_outside_range = self[nextkey_idx:]
-        for frame in filter(has_cel, next_outside_range):
-            cel = frame.cel
-            if cel not in opacities.keys():
-                if frame.is_key:
-                    opacities[cel] = get_opa('next', 'other keys')
-                else:
-                    opacities[cel] = get_opa('next', 'other')
-
-        prev_outside_range = self[:prevkey_idx]
-        for frame in filter(has_cel, prev_outside_range):
-            cel = frame.cel
-            if cel not in opacities.keys():
-                if frame.is_key:
-                    opacities[cel] = get_opa('previous', 'other keys')
-                else:
-                    opacities[cel] = get_opa('previous', 'other')
-
-        visible = {}
-        for cel, opa in opacities.items():
-            visible[cel] = True
-            if opa == 0:
-                visible[cel] = False
-
-#        print opacities, visible
-        return opacities, visible
-
-    def count_cel(self, item):
-        count = 0
-        for f in self:
-            if f.cel and f.cel == item:
-                count += 1
-        return count
-
-def print_list(frames):
-    """
-    Utility funtion for debugging.
-
-    """
-    def info(f):
-        all_info = []
-        if frames.get_selected() == f:
-            all_info.append("selected")
-        if frames.get_previous_key() == f:
-            all_info.append("previous key")
-        if frames.get_next_key() == f:
-            all_info.append("next key")
-        return ', '.join(all_info)
-
-    def key(f):
-        if f.is_key:
-            return '*'
-        return ''
-
-    for n, f in enumerate(frames):
-        print n, key(f), info(f)
-
-        
-__test__ = dict(allem="""
-
-Moving through frames
----------------------
-
->>> frames = FrameList(4)
->>> frames.idx
-0
-
->>> frames.index(frames.get_selected()) # same as frames.idx
-0
-
->>> frames.has_previous()
-False
-
->>> frames.goto_previous()
-"IndexError: Trying to go to previous at the first frame."
-
->>> frames.goto_next()
->>> frames.idx
-1
-
->>> frames.has_previous()
-True
-
->>> frames.has_next()
-True
-
->>> frames.goto_next()
->>> frames.idx
-2
-
->>> frames.goto_previous()
->>> frames.goto_previous()
->>> frames.idx
-0
-
->>> frames.select(0)
->>> frames.idx
-0
-
->>> frames.select(len(frames)-1) # last frame
->>> frames.idx
-3
-
->>> frames.has_next()
-False
-
->>> frames.goto_next()
-"IndexError: Trying to go to next at the last frame."
-
->>> frames.select(100)
-"IndexError: Trying to select inexistent frame."
-
-Using key frames
-----------------
-
->>> frames = FrameList(4)
->>> frames.get_previous_key() == None
-True
-
->>> frames.get_next_key() == None
-True
-
->>> frames[0].set_key()
->>> frames[3].set_key()
->>> frames.get_previous_key() == None
-True
-
->>> frames.index(frames.get_next_key())
-3
-
->>> frames.select(2)
->>> frames.index(frames.get_previous_key())
-0
-
->>> frames.index(frames.get_next_key())
-3
-
->>> frames.has_previous_key()
-True
-
->>> frames.goto_previous_key()
->>> frames.idx
-0
-
->>> frames.has_previous_key()
-False
-
->>> frames.goto_previous_key()
-"IndexError: Trying to go to inexistent previous keyframe."
-
->>> frames.goto_next_key()
->>> frames.has_previous_key()
-True
-
->>> frames.has_next_key()
-False
-
->>> frames.goto_next_key()
-"IndexError: Trying to go to inexistent next keyframe."
-
->>> frames.idx
-3
-
-Adding cels to frames
----------------------
-
->>> frames = FrameList(4)
->>> frames.cel_at(0) == None
-True
-
->>> frames.cel_at(2) == None
-True
-
->>> frames.cel_at(3) == None
-True
-
->>> frames[0].add_cel('a')
->>> frames[1].add_cel('b')
->>> frames[3].add_cel('c')
->>> frames.cel_at(0)
-'a'
-
->>> frames.cel_at(2)
-'b'
-
->>> frames.cel_at(3)
-'c'
-
->>> frames[3].set_key()
->>> frames.cel_for_frame(frames.get_next_key())
-'c'
-
->>> frames.idx
-0
-
-Moving through frames with cels
--------------------------------
-
->>> frames.goto_next(with_cel=True)
->>> frames.idx
-1
-
->>> frames.has_next(with_cel=True)
-True
-
->>> frames.goto_next(with_cel=True)
->>> frames.idx
-3
-
->>> frames.has_next(with_cel=True)
-False
-
->>> frames.goto_next(with_cel=True)
-"IndexError: There is no next frame with cel."
-
->>> frames.goto_previous(with_cel=True)
->>> frames.idx
-1
-
->>> frames.has_previous(with_cel=True)
-True
-
->>> frames.goto_previous(with_cel=True)
->>> frames.idx
-0
-
->>> frames.has_previous(with_cel=True)
-False
-
->>> frames.goto_previous(with_cel=True)
-"IndexError: There is no previous frame with cel."
-
-Testing opacities
------------------
-
->>> active_cels = {'next': True, 'previous': True, \
-                   'next key': False, 'previous key': False, \
-                   'inbetweens': False, 'other keys': False, \
-                   'other': False}
-
->>> frames = FrameList(6, active_cels=active_cels)
->>> frames.get_opacities()
-({}, {})
-
->>> frames[0].add_cel('a')
->>> frames[2].add_cel('b')
->>> frames[4].add_cel('c')
->>> frames.select(2)
->>> set(frames.get_opacities()[0].items()) == set([('a', 0.5), ('b', 1), ('c', 0.5)])
-True
-
-Appending more frames
----------------------
-
->>> frames = FrameList(6)
->>> len(frames)
-6
-
->>> frames.append_frames(6)
->>> len(frames)
-12
-
->>> rem = frames.remove_frames(6)
->>> len(frames)
-6
-
-Inserting frames
-----------------
-
->>> frames = FrameList(4)
->>> frames.idx = 2
->>> len(frames)
-4
-
->>> frames[0].add_cel('a')
->>> frames[2].add_cel('c')
->>> frames.insert_empty_frames(2)
->>> len(frames)
-6
-
->>> frames.cel_at(2)
-'a'
-
->>> frames.cel_at(4)
-'c'
-
->>> frames[2].add_cel('b')
->>> frames.cel_at(0)
-'a'
-
->>> frames.cel_at(2)
-'b'
-
->>> frames.cel_at(4)
-'c'
-
->>> timeline.idx
-2
-
->>> rem = frames.frames_to_remove(2)
->>> rem[0].cel == 'b'
-True
-
->>> rem[1].cel == None
-True
-
->>> rem2 = frames.remove_frames(2)
->>> rem == rem2
-True
-
->>> len(frames)
-4
-
->>> frames.cel_at(0)
-'a'
-
->>> frames.cel_at(2)
-'c'
-
->>> frames.insert_frames(rem)
->>> len(frames)
-6
-
->>> timeline.idx = 5 # at the end
->>> rem3 = frames.remove_frames(1)
->>> len(rem3)
-1
-
->>> len(frames)
-5
->>> timeline.idx
-4
-
->>> rem4 = frames.remove_frames(2)
->>> len(rem4)
-1
-
->>> len(frames)
-4
->>> timeline.idx
-3
-
-Count cels occurrencies
------------------------
-
->>> frames = FrameList(4)
->>> frames[0].add_cel('foo')
->>> frames[1].add_cel('bar')
->>> frames[3].add_cel('foo')
->>> frames.count_cel('foo')
-2
-
->>> frames.count_cel('bar')
-1
-
->>> frames.count_cel('qwe')
-0
-
-Frames that are not considered for onion-skin
----------------------------------------------
-
->>> active_cels = {'next': True, 'previous': True, \
-                   'next key': False, 'previous key': False, \
-                   'inbetweens': False, 'other keys': False, \
-                   'other': False}
-
->>> frames = FrameList(4, active_cels=active_cels)
->>> frames[0].add_cel('a')
->>> frames[1].add_cel('b')
->>> frames[2].add_cel('c')
->>> frames.select(1)
-
->>> frames[0].skip_visible
-False
-
->>> set(frames.get_opacities()[1].items()) == set([('a', True), ('b', True), ('c', True)])
-True
-
->>> frames[0].skip_visible = True
->>> frames[0].skip_visible
-True
-
->>> set(frames.get_opacities()[1].items()) == set([('a', False), ('b', True), ('c', True)])
-True
-
->>> frames[2].toggle_skip_visible()
-
->>> set(frames.get_opacities()[1].items()) == set([('a', False), ('b', True), ('c', False)])
-True
-
->>> frames[1].toggle_skip_visible()  # don't skip current frame
->>> set(frames.get_opacities()[1].items()) == set([('a', False), ('b', True), ('c', False)])
-True
-
-""")
-
-import doctest
-doctest.testmod()

commit 4a18aa0c5b96015a45ae9dc7aa4dd70fc0b76a0f

fix error introduced in d48524fff7

lib/animation.py
@@ -183,10 +183,12 @@ class Animation(object):
                     self.timeline[j].opacity = raster_frames[j]['opacity']
                     self.timeline[j].locked = raster_frames[j]['locked']
                     self.timeline[j].composite = raster_frames[j]['composite']
-                    for i in raster_frames[j]:
-                        if type(i) is not int:
+                    for ui in raster_frames[j]:
+                        try:
+                            i = int(ui)
+                        except ValueError:
                             continue
-                        d = raster_frames[j][i]
+                        d = raster_frames[j][ui]
                         f = self.timeline[j][i]
                         if d['idx'] is not None:
                             if d['idx'] < len(self.doc.layers):